2ae4b4891b0ca0899aa018bcfdbb6a424e029b0c
J. Xavier Atero <xavier.atero@pixel51.net>
2018-08-02 12:37:16 +0200

    Added dimensions for reference

--- HydraulicFormulas.UnitTest/UnitTestColebrookWhite.cs
+++ HydraulicFormulas.UnitTest/UnitTestColebrookWhite.cs
@@ -16,9 +16,9 @@ namespace Automaticu.HydraulicFormulas.UnitTest
       [TestMethod]
       public void TestColebrookWhite()
       {
-         double epsilon_relative_roughness = 0.15;
-         double DH_hydraulic_diameter = 136;
-         double Re_reynolds_number = 1.87e+6;
+         double epsilon_relative_roughness = 0.15; // mm
+         double DH_hydraulic_diameter = 136; // mm
+         double Re_reynolds_number = 1.87e+6; // dimensionless
 
          double colebrook_white_bisection = HydraulicFormulas.CollebrookWhiteBisection(epsilon_relative_roughness, DH_hydraulic_diameter, Re_reynolds_number);
          double colebrook_white_brent = HydraulicFormulas.CollebrookWhiteBrent(epsilon_relative_roughness, DH_hydraulic_diameter, Re_reynolds_number);
